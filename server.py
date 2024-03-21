b9aecf46a29a24800ba985810261e2216f75af2e
NightTrek <Daniels@dual4t.com>
2024-03-21 00:39:32 -0700

    working steam support

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ from flask import Flask, request, jsonify, Response
 from init import init
 
 from vaultAPI.retrival_test import retrive_chunks, ask_question
-from chromaDBScripts.createIndexFromFolder import query_chroma
-from oAI.openai_utils import convert_chunk_to_api, createChunk, createChoices
+from chromaDBScripts.createIndexFromFolder import query_chroma, expand_query
+from oAI.openai_utils import convert_chunk_to_api, createChunk, createChoices, create_chat_message_chunk
 
 
 app = Flask(__name__)
@@ -29,6 +29,7 @@ def chat():
     top_p = data.get('top_p', 1)
     frequency_penalty = data.get('frequency_penalty', 0)
     presence_penalty = data.get('presence_penalty', 0)
+    stream = data.get('stream', False)
 
     if prompt.startswith('/'):
         command = prompt[1:].split(' ')[0]
@@ -41,19 +42,71 @@ def chat():
         else:
             return jsonify({'choices': [{'text': f'Unknown command: {command}. Use /help to see available commands.'}]})
     
+    if stream == False:
+        return jsonify({
+        "id": "chatcmpl-123",
+        "object": "chat.completion",
+        "created": 1677652288,
+        "model": "gpt-3.5-turbo-0125",
+        "system_fingerprint": "fp_44709d6fcb",
+        "choices": [{
+            "index": 0,
+            "message": {
+            "role": "assistant",
+            "content": "\n\nHello there, how may I assist you today?",
+            },
+            "logprobs": "null",
+            "finish_reason": "stop"
+        }],
+        "usage": {
+            "prompt_tokens": 9,
+            "completion_tokens": 12,
+            "total_tokens": 21
+        }
+        })
+        # print("========= RAG CONTEXT: =============")
+        # chroma_response = query_chroma(prompt, 'arxiv-pdfs')
+        # print("============= Chroma responded =============")
+        # print(chroma_response)
+        # print("========= Request: =============")
+
+        # if chroma_response is None:
+        #     print("Chroma returned None")
+        #     chroma_response = ""
+        # completion = client.chat.completions.create(
+        #     model='anthropic/claude-3-sonnet',
+        #     messages=[
+        #         {"role": "user", "content": "CONTEXT: " + str(chroma_response) + " || QUESTION: " + prompt},
+        #     ],
+        # )
+        # return jsonify(completion.model_dump_json())
+    # def generate():
+
+    print("========= Request: =============")
+
+
+        
+
 
     def generate():
+        yield f"data: {json.dumps(create_chat_message_chunk('Let me research this question for a second.'))}\n\n".encode('utf-8')
+        yield f"data: {json.dumps(create_chat_message_chunk('.'))}\n\n".encode('utf-8')
+        yield f"data: {json.dumps(create_chat_message_chunk('.'))}\n\n".encode('utf-8')
+
         print("========= RAG CONTEXT: =============")
-        chroma_response = query_chroma(prompt, 'arxiv-pdfs')
+        expanded_queries = expand_query(prompt)
+        
+        chroma_response = []
+        for query in expanded_queries:
+            
+            yield f'data: {json.dumps(create_chat_message_chunk("New query: " + query))}\n\n'.encode('utf-8')
+            chroma_response.extend(query_chroma(query, 'arxiv-pdfs'))
+        
         print("============= Chroma responded =============")
         print(chroma_response)
-        print("========= Request: =============")
-
         if chroma_response is None:
             print("Chroma returned None")
             chroma_response = ""
-        
-
         for chunk in client.chat.completions.create(
             model='anthropic/claude-3-sonnet',
             messages=[
@@ -61,26 +114,91 @@ def chat():
             ],
             stream=True
         ):
+            print(chunk)
             yield f"data: {json.dumps(convert_chunk_to_api(chunk))}\n\n".encode('utf-8')
 
+    return Response(generate(), mimetype='text/event-stream')
+
+@app.route('/chat', methods=['POST'])
+def streamOnly():
+    data = request.get_json()
+    print("========= DATA: =============")
+    print(data)
+
+    # Extract the parameters from the request
+    model = data.get('model', 'gpt-3.5-turbo-instruct')
+    prompt = next((message['content'] for message in reversed(data['messages']) if message['role'] == 'user'), '')
+    temperature = data.get('temperature', 0.1)
+    max_tokens = data.get('max_tokens', 256)
+    top_p = data.get('top_p', 1)
+    frequency_penalty = data.get('frequency_penalty', 0)
+    presence_penalty = data.get('presence_penalty', 0)
+
+    if prompt.startswith('/'):
+        command = prompt[1:].split(' ')[0]
+        if command == 'help':
+            return jsonify({'choices': [{'text': 'Available commands:\n/help - Show this help message\n/cd <directory> - Change the directory to be indexed'}]})
+        elif command == 'cd':
+            directory = prompt[len(command)+2:]
+            # TODO: Implement logic to change the directory to be indexed
+            return jsonify({'choices': [{'text': f'Changed directory to {directory}'}]})
+        else:
+            return jsonify({'choices': [{'text': f'Unknown command: {command}. Use /help to see available commands.'}]})
+    
+    print("========= RAG CONTEXT: =============")
+    chroma_response = query_chroma(prompt, 'arxiv-pdfs')
+    print("============= Chroma responded =============")
+    print(chroma_response)
+    print("========= Request: =============")
+
+    if chroma_response is None:
+        print("Chroma returned None")
+        chroma_response = ""
+    
+    def generate():
+        for chunk in client.chat.completions.create(
+            model='anthropic/claude-3-sonnet',
+            messages=[
+                {"role": "user", "content": "CONTEXT: " + str(chroma_response) + " || QUESTION: " + prompt},
+            ],
+            stream=True
+        ):
+
+            chunk_response = {
+                "id":chunk.id,
+                "object":"chat.completion.chunk",
+                "created":chunk.created,
+                "model":chunk.model, 
+                "system_fingerprint": chunk.system_fingerprint,
+                "choices":[{
+                    "index":chunk.choices[0].index,
+                    "delta":
+                        {"role":chunk.choices[0].delta.role,
+                         "content":chunk.choices[0].delta.content
+                         },
+                    "logprobs":chunk.choices[0].logprobs,
+                    "finish_reason":chunk.choices[0].finish_reason
+                    }]}
+            print(chunk_response)
+            yield f"data: {chunk_response}\n\n".encode('utf-8')
 
     return Response(generate(), mimetype='text/event-stream')
 
 
-@app.route('/v1/models', methods=['POST'])
+@app.route('/v1/models', methods=['GET'])
 def models():
-    return {
+    return jsonify({
   "object": "list",
   "data": [
-    {
-      "id": "stephen-colbert",
-      "object": "model",
-      "created": 1686935002,
-      "owned_by": "organization-owner"
-    },
+        {
+        "id": "gpt-3.5-turbo",
+        "object": "model",
+        "created": 1686935002,
+        "owned_by": "openai"
+        },
   ],
   "object": "list"
-}
+})
 
 if __name__ == '__main__':
     app.run(port=3592)
