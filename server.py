4bd46b6ac2dab108519c8572378de9a07bc8c06d
NightTrek <Daniels@dual4t.com>
2024-03-21 00:57:25 -0700

    added support for disabling the query expander

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from init import init
 
 from vaultAPI.retrival_test import retrive_chunks, ask_question
 from chromaDBScripts.createIndexFromFolder import query_chroma, expand_query
-from oAI.openai_utils import convert_chunk_to_api, createChunk, createChoices, create_chat_message_chunk
+from oAI.openai_utils import convert_chunk_to_api, createChunk, send_chat_message, create_chat_message_chunk
 
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ default_dir = 'mini-arxiv-pdfs' # Default directory to index
 
 client = init() # set the environment variables
 
+textEpansion = True
 
 @app.route('/v1/chat/completions', methods=['POST'])
 def chat():
@@ -34,68 +35,72 @@ def chat():
     if prompt.startswith('/'):
         command = prompt[1:].split(' ')[0]
         if command == 'help':
-            return jsonify({'choices': [{'text': 'Available commands:\n/help - Show this help message\n/cd <directory> - Change the directory to be indexed'}]})
+            return jsonify(send_chat_message("""
+                Available commands:\n/
+                help - Show this help message\n/
+                cd <directory> - Change the directory to be indexed
+                expand_query - Enable or disable llm based query expansion
+            """))
         elif command == 'cd':
             directory = prompt[len(command)+2:]
             # TODO: Implement logic to change the directory to be indexed
-            return jsonify({'choices': [{'text': f'Changed directory to {directory}'}]})
+            return jsonify(send_chat_message("TODO implement changing directory"))
+        elif command == 'expand_query':
+            if textEpansion:
+                textEpansion = False
+            else:
+                textEpansion = True
+            return jsonify(send_chat_message("Query expansion toggled"))
         else:
-            return jsonify({'choices': [{'text': f'Unknown command: {command}. Use /help to see available commands.'}]})
+            return jsonify(send_chat_message("unkown command use help for a list of commands"))
     
     if stream == False:
-        return jsonify({
-        "id": "chatcmpl-123",
-        "object": "chat.completion",
-        "created": 1677652288,
-        "model": "gpt-3.5-turbo-0125",
-        "system_fingerprint": "fp_44709d6fcb",
-        "choices": [{
-            "index": 0,
-            "message": {
-            "role": "assistant",
-            "content": "\n\nHello there, how may I assist you today?",
-            },
-            "logprobs": "null",
-            "finish_reason": "stop"
-        }],
-        "usage": {
-            "prompt_tokens": 9,
-            "completion_tokens": 12,
-            "total_tokens": 21
-        }
-        })
-        # print("========= RAG CONTEXT: =============")
-        # chroma_response = query_chroma(prompt, 'arxiv-pdfs')
-        # print("============= Chroma responded =============")
-        # print(chroma_response)
-        # print("========= Request: =============")
+        # return jsonify({
+        # "id": "chatcmpl-123",
+        # "object": "chat.completion",
+        # "created": 1677652288,
+        # "model": "gpt-3.5-turbo-0125",
+        # "system_fingerprint": "fp_44709d6fcb",
+        # "choices": [{
+        #     "index": 0,
+        #     "message": {
+        #     "role": "assistant",
+        #     "content": "\n\nHello there, how may I assist you today?",
+        #     },
+        #     "logprobs": "null",
+        #     "finish_reason": "stop"
+        # }],
+        # "usage": {
+        #     "prompt_tokens": 9,
+        #     "completion_tokens": 12,
+        #     "total_tokens": 21
+        # }
+        # })
+        print("========= RAG CONTEXT: =============")
+        chroma_response = query_chroma(prompt, 'arxiv-pdfs')
+        print("============= Chroma responded =============")
+        print(chroma_response)
+        print("========= Request: =============")
 
-        # if chroma_response is None:
-        #     print("Chroma returned None")
-        #     chroma_response = ""
-        # completion = client.chat.completions.create(
-        #     model='anthropic/claude-3-sonnet',
-        #     messages=[
-        #         {"role": "user", "content": "CONTEXT: " + str(chroma_response) + " || QUESTION: " + prompt},
-        #     ],
-        # )
-        # return jsonify(completion.model_dump_json())
-    # def generate():
+        if chroma_response is None:
+            print("Chroma returned None")
+            chroma_response = ""
+        completion = client.chat.completions.create(
+            model='anthropic/claude-3-sonnet',
+            messages=[
+                {"role": "user", "content": "CONTEXT: " + str(chroma_response) + " || QUESTION: " + prompt},
+            ],
+        )
+        return jsonify(completion.model_dump_json())
 
     print("========= Request: =============")
-
-
-        
-
-
     def generate():
         yield f"data: {json.dumps(create_chat_message_chunk('Let me research this question for a second.'))}\n\n".encode('utf-8')
         yield f"data: {json.dumps(create_chat_message_chunk('.'))}\n\n".encode('utf-8')
         yield f"data: {json.dumps(create_chat_message_chunk('.'))}\n\n".encode('utf-8')
 
         print("========= RAG CONTEXT: =============")
-        expanded_queries = expand_query(prompt)
-        
+        expanded_queries = [prompt] if not textEpansion else expand_query(prompt)
         chroma_response = []
         for query in expanded_queries:
             
