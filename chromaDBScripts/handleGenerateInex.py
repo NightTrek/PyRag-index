645e1728a531b8eebda685079cd9d27b95bd7309
NightTrek <Daniels@dual4t.com>
2024-03-21 15:51:35 -0700

    updated indexing scripts

--- chromaDBScripts/handleGenerateInex.py
+++ chromaDBScripts/handleGenerateInex.py
@@ -1,4 +1,12 @@
+import sys
 from createIndexFromFolder import generate_index_from_folder
 
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide the directory as a command-line argument.")
+        sys.exit(1)
+    directory = sys.argv[1]
+    generate_index_from_folder(directory, directory)
+
 if __name__ == '__main__':
-    generate_index_from_folder("arxiv-pdfs", "arxiv-pdfs")
+    main()
