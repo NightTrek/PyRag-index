54c646d91dd039e5060828de3fb2052d40f53ea2
NightTrek <Daniels@dual4t.com>
2024-03-27 17:57:24 -0700

    finalized file chipper support

--- vaultAPI/file_chipper.py
+++ vaultAPI/file_chipper.py
@@ -1,6 +1,7 @@
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 import os
+import time
 
 api_url = "https://filechipper.com/upload"
 
@@ -26,24 +27,31 @@ class FileChipperResponse(BaseModel):
     positionData: list[PositionData]
 
 def chunk_files(file_path: str, batch_size: int = 4):
-
     multiPartForms = []
-    file_fields = []
-    for file_name in os.listdir(file_path):
-        file_fields.append(
-            ('files', (file_name, open(os.path.join(file_path, file_name), 'rb')))
-        )
-        if len(file_fields) > batch_size:
-            multiPartForms.append(MultipartEncoder(fields=file_fields))
-            file_fields = []
-    # Request chunking for the split up batches of files        
-    results_chunks = [FileChipperResponse]
+    total_files = len(os.listdir(file_path))
+
+    print(f"total files in folder: {total_files}")
+    for i in range(0, total_files, batch_size):
+        file_fields = []
+        for file_name in os.listdir(file_path)[i:i+batch_size]:
+            file_fields.append(
+                ('files', (file_name, open(os.path.join(file_path, file_name), 'rb')))
+            )
+        multiPartForms.append(MultipartEncoder(fields=file_fields))
+    # Request chunking for the split up batches of files
+    print(f"chunking {len(multiPartForms)} forms")        
+    results_chunks = []
     for form in multiPartForms:
+        print('Next Batch')
+
         try:
             response = requests.post(api_url, data=form, headers={'Content-Type': form.content_type}, timeout=60)
             response.raise_for_status()
-            json_results = response.json()
-            results_chunks.extend(json_results.results)
+            json_results = response.json()['results']
+            for chunkArray in json_results:
+                results_chunks.extend([FileChipperResponse(text=result['text'], positionData=[PositionData(page_idx=pos['page_idx'], block_idx=pos['block_idx'], bbox=[int(coord) for coord in pos['bbox']], tag=pos['tag']) for pos in result['positionData']]) for result in chunkArray])
+
+            print(f"Total_file_chunks = {len(results_chunks)}")
         except requests.exceptions.Timeout:
             print("Request timed out. Retrying...")
             continue
--- vaultAPI/remove_all_filesDANGER.py
+++ vaultAPI/remove_all_filesDANGER.py
@@ -1,18 +1,24 @@
-from init import init
 import requests
 import os
+import sys
 
-init()
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import init
+
+
+init.init()
 
 api_key = os.environ['VAULT_API_KEY']
 url = f"https://vault.pash.city/api/get/uploads?api_key={api_key}"
 
 response = requests.get(url)
 file_list = response.json()
+print(file_list)
 
 remove_list = []
 for file in file_list:
-    remove_list.append(file.id)
+    remove_list.append(file['id'])
 
 
 url = "https://vault.pash.city/api/delete/files?api_key=" + api_key
